55e1f2241c243d1b3d926c34449fe73be4b8cdba
Ganesh Govind J <codemuscle@Ganeshs-MacBook-Air.local>
2023-06-12 16:12:30 +0530

    base files

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,14 @@
-// import { useState } from 'react';
-import './App.css'
+import { useState } from 'react';
+import './index.css'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <>
-      
-    </>
+      <div className="p-10 self-center flex flex-col gap-10 bg-gray-400 text-center">
+        <p className="text-black font-bold text-[40px] bg-transparent">Hi, I'm Ganesh</p>
+        <button className="p-4 border-black bg-blue-400 font-semibold text-[20px] rounded-lg w-[200px] self-center">Change my name!</button>
+      </div>
   )
 }
 
